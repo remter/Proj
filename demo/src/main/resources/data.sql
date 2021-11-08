65d5dc69f9cbb007cd7af6a157b608c4c8763694
Zyter <60459687+remter@users.noreply.github.com>
2021-11-08 15:19:40 -0800

    updated data and schema tables

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into inventory ('name', 'serialNum', 'type', 'model', 'maker', 'description', 'ownerid') VALUES
+('Blackberry a40', 's4302er1', 'phone', 'a4023', 'Blackberry', 'Blackberry phone with camera and keypad', '1');
+
+insert into users('name', 'username', 'role') VALUES
+('Bob barker', 'bbar001', 'owner'),
+('Billy Jeen', 'bjee001', 'manager'),
+('Paul Walker', 'pwal001', 'admin'),
+('Dead Pool', 'dpoo001', 'engineer'),
+('Linus Touch', 'ltou001', 'user');
